9eaed836592332d70ea9db1c84b368c4b23f3dec
Francisco Pérez <devfcph@gmail.com>
2025-04-09 22:47:39 -0600

    Created Client entity

--- src/main/java/com/fcph/challengeaplazo/entity/Client.java
+++ src/main/java/com/fcph/challengeaplazo/entity/Client.java
@@ -1,4 +1,27 @@
 package com.fcph.challengeaplazo.entity;
 
+import lombok.*;
+import jakarta.persistence.*;
+import org.springframework.boot.context.properties.bind.Name;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "clients")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDate birthDate;
+
+    private Double creditLine;
 }
